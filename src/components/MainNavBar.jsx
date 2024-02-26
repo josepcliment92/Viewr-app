98ac0abe8da28e0cf2dd9fbe0daab9a4dd043f23
Josep Climent <josepcliment92@gmail.com>
2024-02-26 12:31:22 +0100

    añadir paginas home y list shows

--- src/components/MainNavBar.jsx
+++ src/components/MainNavBar.jsx
@@ -4,7 +4,6 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from "react-bootstrap/Offcanvas"; 
 
 function MainNavBar() {
   return (
