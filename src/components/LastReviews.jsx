f1f2d88fc905e19ee93144e25b5304f42658244b
Abranucu <abrahamcastiellobarbas@gmail.com>
2024-02-29 10:25:28 +0100

    last reviews responsive

--- src/components/LastReviews.jsx
+++ src/components/LastReviews.jsx
@@ -1,79 +1,132 @@
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 
-
 function LastReviews(props) {
   const review = props.review;
-  const date = new Date(review.date)
-  const dateToShow = `Date: ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}, 
-  Time: ${date.getHours()}:${date.getMinutes()}`;
-  
+  const date = new Date(review.date);
+  const dateToShow = `Date: ${date.getDate()}/${
+    date.getMonth() + 1
+  }/${date.getFullYear()},`;
+  const timeToShow = `Time: ${date.getHours()}:${date.getMinutes()}`;
+
   return (
-      <div>
-        <Link
-          to={`/list-shows/${review.showID}`}
-          onClick={(e) => handleSubmit(review.showID)}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        minWidth: "200px",
+        backgroundColor: "white",
+        margin: "5px",
+        borderRadius: "10px",
+      }}
+    >
+      <Link
+        to={`/list-shows/${review.showID}`}
+        onClick={(e) => handleSubmit(review.showID)}
+        style={{
+          textDecoration: "none",
+          width: "200px",
+          maxWidth: "100%",
+          maxHeight: "400px",
+          margin: "5px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Card style={{ flex: 1 }}>
+          <div style={{ height: "300px", position: "relative" }}>
+            <Card.Img
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                borderRadius: "10px",
+              }}
+              variant="top"
+              src={review.showImage}
+            />
+          </div>
+          <Card.Body
+            style={{
+              height: "100px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Card.Title
+              style={{
+                color: "black",
+                fontSize: "16px",
+                lineHeight: "20px",
+                textAlign: "center",
+              }}
+            >
+              {review.showTitle}
+            </Card.Title>
+          </Card.Body>
+        </Card>
+      </Link>
+      <Card
+        bg="secondary"
+        style={{
+          width: "200px",
+          maxWidth: "100%",
+          maxHeight: "400px",
+          margin: "5px",
+          color: "white",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Card.Header
           style={{
-            textDecoration: "none",
+            height: "40px",
             display: "flex",
-            flexDirection: "row",
-            border: "0px",
-            borderRadius: "1rem",
-            margin: "5px",
-            backgroundColor: "white",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <div>
-            <Card style={{ width: "12rem", margin: "0.5em", border: "0px" }}>
-              <Card.Img
-                style={{
-                  height: "18rem",
-                  objectFit: "cover",
-                  borderRadius: "10px",
-                }}
-                variant="top"
-                src={review.showImage}
-              />
-              <Card.Body
-                style={{
-                  height: "6rem",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                }}
-              >
-                <Card.Title style={{ color: "black" }}>
-                  {review.showTitle}
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </div>
-          <div>
-            <Card
-              bg="secondary"
-              style={{
-                width: "18rem",
-                height: "24rem",
-                margin: "0.5em",
-                color: "white",
-              }}
-              className="review-home"
-            >
-              <Card.Header>{review.username}</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  <strong>Rating: </strong>
-                  {review.rating}
-                </Card.Title>
-                <Card.Text>
-                  <strong>Review: </strong> {review.review}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>{dateToShow}</Card.Footer>
-            </Card>
-          </div>
-        </Link>
-      </div>
+          {review.username}
+        </Card.Header>
+        <Card.Body
+          style={{
+            height: "150px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Card.Title style={{ marginBottom: "5px", textAlign: "center" }}>
+            <strong>Rating: </strong>
+            {review.rating}
+          </Card.Title>
+          <Card.Text
+            style={{
+              marginBottom: "5px",
+              textAlign: "center",
+              overflowY: "auto",
+            }}
+          >
+            <strong>Review: </strong> {review.review}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer
+          style={{
+            height: "60px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {dateToShow}
+          <br />
+          {timeToShow}
+        </Card.Footer>
+      </Card>
+    </div>
   );
 }
 
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -5,10 +5,10 @@ import sloganLogo2 from "../assets/img/slogan-logo-2.png";
 
 function Start() {
   return (
-    <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+    <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
       <Link to={"/home"}>
         <button style={{backgroundColor: "transparent", borderRadius: "50px", border: "3px solid white"}}>
-          <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{display: "flex", flexDirection: "column"}}>
             <img src={appLogo} alt="app-logo" style={{maxWidth: "100%", height: "auto", display: "block", margin: "0 auto"}}/>
             <img src={sloganLogo2} alt="slogan-logo" style={{maxWidth: "100%", height: "auto", display: "block", margin: "0 auto"}}/>
           </div>
