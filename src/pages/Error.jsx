31e42b6ee9625d8cf2762f69c67f5038ab0f130f
Josep Climent <josepcliment92@gmail.com>
2024-02-29 10:23:09 +0100

    paginas about, error y not found

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 function Error() {
   return (
     <div>
-      Algo ha fallado. ¿Quieres volver al inicio? 
-     <Link to={"/home"}><button>Clica aquí</button></Link> 
+      <h2>Oh No! It looks like something went wrong</h2>
+      <br />
+      <p>
+        We're sorry 😢 The page you are trying to access may not exist, or there
+        may be a temporary issue with our server.
+      </p>
+      <p>
+        We invite you to go back to our Homepage and keep enjoying the Viewr
+        content. Thank you!
+      </p>
+      <p>👇</p>
+      <Link to={"/home"}>
+        <Button>Go back to Homepage</Button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 function NotFound() {
   return (
     <div>
-      Ha habido un error; nuestros programadores están trabajando en ello. ¿Quieres volver a la página inicial?
-      <Link to={"/home"}><button>Clica aquí</button></Link>
+      <h2>
+        <span style={{ color: "red" }}>404</span> - Page Not Found
+      </h2>
+      <br />
+      <p>
+        We're sorry 😢 It looks like the page you're looking for doesn't exist.
+      </p>
+      <p>
+        The page may have been removed or it could be temporarily unavailable.
+        Try again in a few minutes!
+      </p>
+      <p>
+        You can also go back to our Homepage and keep enjoying the Viewr
+        content. Thank you for your patience.
+      </p>
+      <p>👇</p>
+      <Link to={"/home"}>
+        <Button>Go back to Homepage</Button>
+      </Link>
     </div>
   );
 }
